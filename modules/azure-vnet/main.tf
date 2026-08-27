4c710d20ae3a10dae88646833716c5367d7bf41d
DeeSanas <106792615+DeeSanas@users.noreply.github.com>
2026-08-27 14:53:38 +0700

    Add reusable Azure VNet module

---
+++ modules/azure-vnet/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0, < 5.0"
+    }
+  }
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "this" {
+  for_each = var.subnets
+
+  name                 = each.key
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = [each.value]
+}
